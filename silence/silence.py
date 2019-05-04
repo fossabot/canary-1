82c351ad9de71688055c67d1214006e57316229c
Mike McGarry <mikemcgarry@live.com.au>
2019-05-04 16:03:39 +0100

    DEV-0000 Started work on Dockerfiles and initial file logic

--- silence/silence.py
+++ silence/silence.py
@@ -2,3 +2,19 @@
 This file handles generating appropriate notifications from the latest air
 pollution data
 """
+
+# Import the air pollution data
+while True:
+    try:
+        air_pollution = pd.read_csv("./data/air-pollution-data.csv")
+        break
+    except:
+        print ('File does not exist yet, waiting 30 secs before retrying')
+        time.sleep(30)
+
+"""
+Assumptions:
+- Times are taken on the hour.
+- The average level changes hourly.
+"""
+# Logic to handle triggering notifications on each of the three topics
