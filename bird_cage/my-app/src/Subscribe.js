592c024ab3be6e8b165c5c03539d90b42c13b752
Michael McGarry <mike.mcgarry@finbourne.com>
2019-07-03 21:08:17 +0100

    Updated the front end application design

--- bird_cage/my-app/src/Subscribe.js
+++ bird_cage/my-app/src/Subscribe.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import logo from './img/canary.png';
-import { Col, Button, Form, FormGroup, Label, Input, FormText, Row, CardImg } from 'reactstrap';
+import { Col, Button, Form, FormGroup, Label, Input, FormText, Row, CardImg, Badge } from 'reactstrap';
+import Container from "reactstrap/es/Container";
+
+
+const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 50
+        }}
+    />
+);
+
 
 class MyForm extends React.Component {
   constructor() {
@@ -20,44 +33,67 @@ class MyForm extends React.Component {
 
   render() {
     return (
-        <div>
-            <Col sm={6}>
-            <div>
-            <img src = {logo} align="left"/>
-            </div>
-            <div>
-                <p>Air pollution is a big problem in London. After suffering from the affects first hand, I was
-                determined to help make life easier for others that might also be suffering from the poor air quality
-                of the city. If you find that some days you feel a little short of breath for no reason, or perhaps you
-                have had some unexplained chest pain or just a general feeling of being unwell you may be affected by
-                air pollution.</p>
-            </div>
-            <div>
-
-            <Form onSubmit={this.handleSubmit}>
-                <FormGroup row>
-                    <Label for="phone" sm={2}>Phone</Label>
-                    <Col sm={6}>
-                        <Input id="phone" name="phone" type="text" placeholder="07719143007"/>
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for="topic" sm={2}>Alert level</Label>
-                    <Col sm={6}>
-                        <Input type="select" name="topic" id="topic">
-                        <option value="green">Hourly alerts</option>
-                        <option value="yellow">Highly sensitive</option>
-                        <option value="amber">Moderately sensitive</option>
-                        <option value="red">No known sensitivity</option>
-                        </Input>
-                    </Col>
-                </FormGroup>
-                <Button color="danger" align='right'>Subscribe!</Button>
-            </Form>
-
-            </div>
+        <Container fluid="True">
+            <Row className="mt-1">
+                <Col className="center"><img src = {logo}/><Badge color="danger">Beta</Badge></Col>
+                <Col className="flex-xs-middle">
+                    <h1 className="header-font">
+                        <br></br>
+                        <p>Chirping Canary</p>
+                        <p className="arial">Air Pollution Early Warning System</p>
+                    </h1>
                 </Col>
-        </div>
+            </Row>
+
+            <Row className="mt-1">
+                <Col><ColoredLine color="gold" /></Col>
+            </Row>
+
+            <Row className="mt-1">
+                <Col>
+                    <p>
+                    Air pollution is a big problem in London.
+                    </p>
+                    <p>
+                    If you find that some days you feel a little short of breath for no reason, or perhaps you
+                    have had some unexplained chest pain or just a general feeling of being unwell you may be affected by
+                    air pollution.
+                    </p>
+                    <p>
+                    It doesn't have to be this way! With Chirping Canary on your side you don't need to be caught off-guard
+                    by a bad pollution day. Like the canary in the coal mine, Chirping Canary is here to warn you when
+                    the pollution levels in London reach unacceptable levels.
+                    </p>
+                    <p>
+                    Enter your phone number and select your sensitivity level to air pollution to receive free notifcations
+                    every time the air pollution reaches a level that affects you.
+                    </p>
+                </Col>
+
+                <Col>
+                    <Form onSubmit={this.handleSubmit}>
+                        <FormGroup row>
+                            <Label for="phone" sm={2}>Phone</Label>
+                            <Col sm={6}>
+                                <Input id="phone" name="phone" type="text" placeholder="07719143007"/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="topic" sm={2}>Alert level</Label>
+                            <Col sm={6}>
+                                <Input type="select" name="topic" id="topic">
+                                    <option value="green">Hourly alerts</option>
+                                    <option value="yellow">Highly sensitive</option>
+                                    <option value="amber">Moderately sensitive</option>
+                                    <option value="red">No known sensitivity</option>
+                                </Input>
+                            </Col>
+                        </FormGroup>
+                        <Button color="danger" align='right'>Subscribe!</Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
   }
 }
