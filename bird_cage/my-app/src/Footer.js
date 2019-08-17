aed48b0b037d9f423106cc063d09eab606569b1f
Mike McGarry <mikemcgarry@live.com.au>
2019-08-17 12:35:52 +0100

    LogoAttribution: Added attribution for the logo

--- bird_cage/my-app/src/Footer.js
+++ bird_cage/my-app/src/Footer.js
@@ -4,9 +4,9 @@ import PrivacyPolicy from './pdf/Chirping_Canary_Privacy_Policy.pdf';
 //import CookiePolicy from './pdf/Chirping_Canary_Cookie_Policy.pdf';
 import { Col, Row, Container} from 'reactstrap';
 
-function PDF(props) {
+function Link(props) {
   return (
-    <a href = {props.pdf} target = "_blank" rel="noopener noreferrer">{props.pdfName}</a>
+    <a href = {props.link} target = "_blank" rel="noopener noreferrer">{props.linkText}</a>
   );
 }
 
@@ -14,10 +14,14 @@ class Footer extends Component {
   render() {
 
     let privacyPolicy
+    let imageAttributionFreePik
+    let imageAttributionFlatIcon
     //let termsConditions
     //let cookiePolicy
 
-    privacyPolicy = <PDF pdf={PrivacyPolicy} pdfName="Privacy Policy"/>
+    privacyPolicy = <Link link={PrivacyPolicy} linkText="Privacy Policy"/>
+    imageAttributionFreePik = <Link link="https://www.freepik.com/" linkText="Freepik"/>
+    imageAttributionFlatIcon = <Link link="www.flaticon.com/" linkText="www.flaticon.com"/>
     //termsConditions = <PDF pdf={TermsConditions} pdfName="Terms & Conditions"/>
     //cookiePolicy = <PDF pdf={CookiePolicy} pdfName="Cookie Policy"/>
 
@@ -27,6 +31,11 @@ class Footer extends Component {
               <Col className="flex-xs-middle">
                 {privacyPolicy}
               </Col>
+          </Row>
+          <Row className="mt-1">
+            <Col className="flex-xs-middle">
+              <p><font size="1">Chirping Canary Icon made by {imageAttributionFreePik} from {imageAttributionFlatIcon}</font></p>
+            </Col>
           </Row>
       </Container>
     );
