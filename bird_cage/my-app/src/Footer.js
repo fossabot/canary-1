26a44e87f5d647d9a5a4fdd2f72080115379da63
Mike McGarry <mikemcgarry@live.com.au>
2019-09-15 22:07:24 +0100

    GoogleTagManager Added in a cookie consent banner

--- bird_cage/my-app/src/Footer.js
+++ bird_cage/my-app/src/Footer.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import PrivacyPolicy from './pdf/Chirping_Canary_Privacy_Policy.pdf';
 //import TermsConditions from './pdf/Chirping_Canary_Terms_Conditions.pdf';
-//import CookiePolicy from './pdf/Chirping_Canary_Cookie_Policy.pdf';
+import CookiePolicy from './pdf/Chirping_Canary_Cookie_Policy.pdf';
 import { Col, Row, Container} from 'reactstrap';
+import CookieConsent from "react-cookie-consent";
 
 function Link(props) {
   return (
@@ -14,12 +15,14 @@ class Footer extends Component {
   render() {
 
     let privacyPolicy
+    let cookiePolicy
     let imageAttributionFreePik
     let imageAttributionFlatIcon
     //let termsConditions
     //let cookiePolicy
 
     privacyPolicy = <Link link={PrivacyPolicy} linkText="Privacy Policy"/>
+    cookiePolicy = <Link link={CookiePolicy} linkText="Cookie Policy"/>
     imageAttributionFreePik = <Link link="https://www.freepik.com/" linkText="Freepik"/>
     imageAttributionFlatIcon = <Link link="www.flaticon.com/" linkText="www.flaticon.com"/>
     //termsConditions = <PDF pdf={TermsConditions} pdfName="Terms & Conditions"/>
@@ -29,7 +32,7 @@ class Footer extends Component {
       <Container fluid="true">
           <Row className="mt-1">
               <Col className="flex-xs-middle">
-                {privacyPolicy}
+                {privacyPolicy} | {cookiePolicy}
               </Col>
           </Row>
           <Row className="mt-1">
@@ -37,7 +40,12 @@ class Footer extends Component {
               <p><font size="1">Chirping Canary Icon made by {imageAttributionFreePik} from {imageAttributionFlatIcon}</font></p>
             </Col>
           </Row>
+          <CookieConsent buttonText="Ok ">
+            This website uses cookies to enhance your user experience. Read more in the Chirping Canary {cookiePolicy} and {privacyPolicy}.
+            If you continue to use this site we will assume that you are happy with this.
+          </CookieConsent>
       </Container>
+
     );
   }
 }
